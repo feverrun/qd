04e5f4257e211c7e684f9f9544f681b2fc776dbb
feverrun <209981505@qq.com>
2021-09-01 12:12:00 +0800

    update

---
+++ abooky.js
@@ -0,0 +1,20 @@
+const rules = {
+  name: "【阅次元论坛】： ",
+  type: 2, //签到类型 2 需要formhash
+  op: "签到",
+  url1: "https://www.abooky.com/plugin.php?id=k_misign:sign", //用于获取formhash的链接
+  cookie: config.abooky.cookie,
+  reg1: 'formhash=(.+)"', //formhash正则
+  verify: "没有帐号", //验证cookie状态
+  signmethod: "get", //签到请求方式 get/post
+  signurl: "https://www.abooky.com/plugin.php?id=k_misign:sign&operation=qiandao&format=button&formhash=@formhash", //签到链接
+  reg2: "", //重复签到判断
+  reg3: "", //签到成功判断
+  info: "", //签到成功返回信息
+};
+
+async function abooky(){
+const template = require("../Template");
+return rules.name+await template(rules)
+}
+module.exports=abooky
